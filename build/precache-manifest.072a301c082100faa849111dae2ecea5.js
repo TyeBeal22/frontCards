67aadabbd7a3732df0e222d1207114753e388b28
Darth Tye <53497335+TyeBeal22@users.noreply.github.com>
2021-01-15 19:09:14 -0500

    Add files via upload
    layout perfect

---
+++ build/precache-manifest.072a301c082100faa849111dae2ecea5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306d4392f91e183a32aadfd402a0c337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b950ba7df74c1651e3",
+    "url": "/static/css/main.b0b4012b.chunk.css"
+  },
+  {
+    "revision": "d5b38fd78a6be0028c21",
+    "url": "/static/js/2.1e52796d.chunk.js"
+  },
+  {
+    "revision": "a1b950ba7df74c1651e3",
+    "url": "/static/js/main.c5f9d9e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9a771a6d9bb4807479659fca1f8b7a7",
+    "url": "/static/media/Sports.a9a771a6.png"
+  },
+  {
+    "revision": "74d27959c84bff342d1b0b05c02c78b8",
+    "url": "/static/media/background.74d27959.png"
+  },
+  {
+    "revision": "4a8f2e3848093000305d8959f0bcd66a",
+    "url": "/static/media/freak.4a8f2e38.svg"
+  },
+  {
+    "revision": "0c2931ee6dd79213491125dd96bb33f5",
+    "url": "/static/media/socials.0c2931ee.svg"
+  }
+]);
